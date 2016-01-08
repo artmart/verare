3d82a0dae9228c622b05decbcf8e93f8828dbbd6
artmart <artrmart@gmail.com>
2016-01-08 23:49:30 +0400

    some update

--- protected/views/userRole/_search.php
+++ protected/views/userRole/_search.php
@@ -22,7 +22,9 @@
 	</div>
 
 	<div class="row">
+        <div class="span3">
 		<?php echo $form->label($model,'user_role'); ?>
+        </div>
 		<?php echo $form->textField($model,'user_role',array('size'=>60,'maxlength'=>255)); ?>
 	</div>
 
@@ -52,52 +54,72 @@
 	</div>
 
 	<div class="row">
+        <div class="span3">
 		<?php echo $form->label($model,'ledger_access_level'); ?>
+        </div>
 		<?php echo $form->textField($model,'ledger_access_level'); ?>
 	</div>
 
 	<div class="row">
+        <div class="span3">
 		<?php echo $form->label($model,'users_access_level'); ?>
+        </div>
 		<?php echo $form->textField($model,'users_access_level'); ?>
 	</div>
 
 	<div class="row">
+        <div class="span3">
 		<?php echo $form->label($model,'user_roles_access_level'); ?>
+        </div>
 		<?php echo $form->textField($model,'user_roles_access_level'); ?>
 	</div>
 
 	<div class="row">
+        <div class="span3">
 		<?php echo $form->label($model,'portfolios_access_level'); ?>
+        </div>
 		<?php echo $form->textField($model,'portfolios_access_level'); ?>
 	</div>
 
 	<div class="row">
+        <div class="span3">
 		<?php echo $form->label($model,'instruments_access_level'); ?>
+        </div>
 		<?php echo $form->textField($model,'instruments_access_level'); ?>
 	</div>
 
 	<div class="row">
+        <div class="span3">
 		<?php echo $form->label($model,'counterparties_access_level'); ?>
+        </div>
 		<?php echo $form->textField($model,'counterparties_access_level'); ?>
 	</div>
 
 	<div class="row">
+        <div class="span3">
 		<?php echo $form->label($model,'documents_access_level'); ?>
+        </div>
 		<?php echo $form->textField($model,'documents_access_level'); ?>
 	</div>
 
 	<div class="row">
+        <div class="span3">
 		<?php echo $form->label($model,'prices_access_level'); ?>
+        </div>
 		<?php echo $form->textField($model,'prices_access_level'); ?>
 	</div>
 
 	<div class="row">
+        <div class="span3">
 		<?php echo $form->label($model,'audit_trails_access_level'); ?>
+        </div>
 		<?php echo $form->textField($model,'audit_trails_access_level'); ?>
 	</div>
 
 	<div class="row">
+        <div class="span3">
 		<?php echo $form->label($model,'grouping_access_level'); ?>
+        </div>
 		<?php echo $form->textField($model,'grouping_access_level'); ?>
 	</div>
 

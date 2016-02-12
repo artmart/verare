ff726373474c1a44ce5b98892a73ab1544218a87
artmart <artrmart@gmail.com>
2016-02-12 22:31:31 +0400

    Overview  page

--- protected/views/site/admin.php
+++ protected/views/site/admin.php
@@ -4,21 +4,430 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h3> <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h3>
-</br>
-<h3>Admin panel first page</h4>
-<h3>Graphes, charts, tables,....available for user</h4>
+<h3> <i><?php //echo CHtml::encode(Yii::app()->name); ?></i></h3>
 
-</br>
-<?php  //echo CHtml::link('Users',array('/user/admin')); ?>
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>Overview
+            <small>
+                <?php
+                  //  echo $_SESSION["company"];
+                ?> 
+            </small>
+            </h1>
+                    <ol class="breadcrumb">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <?php
+                             //       echo $_SESSION["nowPort"];
+                                ?>
+                             <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <?php
+                         //           foreach($_SESSION["portfolios"] as $p)
+                         //           {
+                          //              echo "<li><a href='#'>" . $p. "</a></li>";
+                          //          }
+                                ?> 
+                                <li class="divider"></li>
+                                <li><a href="#">Settings </a></li>
+                                <li><a href="v_recalc.php">ReCalc</a></li>
+                                <li class="divider"></li>
+                                <li><a href="v_disconnect.php">Logout </a></li>
+                            </ul>
+                        </li>
+                    </ol>
+        </section>
 
-</br>
-<?php  //echo CHtml::link('Ոլորտներ',array('/industries')); ?>
-</br>
-<?php  //echo CHtml::link('Ճյուղեր',array('/subIndustries')); ?>
-</br>
-<?php  //echo CHtml::link('Ցուցանիշներ',array('/data')); ?>
-</br>
-<?php  //echo CHtml::link('Չափման միավորները',array('/measureUnits')); ?>
 
-<?php // echo CHtml::link('Օգտագործման ցուցումներ',array('../downloads/help.pdf'), array("target"=>"_blank")); ?>
+        <!-- Main content -->
+        <section class="content">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="box box-danger">
+			  
+                <div class="box-footer">
+                  <div class="row">
+                    <div class="col-sm-3 col-xs-6">
+                      <div class="description-block border-right">
+                        <span class="description-text">MARKET VALUE</span><p>
+                        <span class="description-percentage text-black"><b><?php // echo $index_nav ?></b></span>
+                      </div><!-- /.description-block -->
+                    </div><!-- /.col -->
+                    <div class="col-sm-3 col-xs-6">
+                      <div class="description-block border-right">
+                        <span class="description-text">O/N P/L</span><p>
+                              <?php
+                              /*
+                                  $pnl = GetPnL($dtmax,'Index',$_SESSION['company']);
+                                  if($pnl >= 0)
+                                  {
+                                      echo "<span class='description-percentage text-green'><i class='fa fa-caret-up'></i> " . number_format($pnl) . "</span>";
+                                  }
+                                  else
+                                  {
+                                      echo "<span class='description-percentage text-red'><i class='fa fa-caret-down'></i> " . number_format($pnl) . "</span>";
+                                  }
+                                  */
+                              ?>
+                      </div><!-- /.description-block -->
+                    </div><!-- /.col -->
+                    <div class="col-sm-3 col-xs-6">
+                      <div class="description-block border-right">
+                        <span class="description-text">RETURN All Time</span><p>
+                        <span class="description-percentage text-black"><?php //echo number_format($rall, 2); ?>%</span>
+                      </div><!-- /.description-block -->
+                    </div><!-- /.col -->
+                    <div class="col-sm-3 col-xs-6">
+                      <div class="description-block border-right">
+                        <span class="description-text">RETURN YTD</span><p>
+                        <span class="description-percentage text-black"><?php //echo number_format($rytd, 2); ?>%</span>
+                      </div><!-- /.description-block -->
+                    </div><!-- /.col -->
+                  </div><!-- /.row -->
+                </div><!-- /.box-footer -->
+			  </div><!-- /.box -->
+			</div><!-- /.col -->
+		  </div><!-- /.row -->
+			  
+			  
+          <div class="row">
+            <div class="col-md-12">
+              <div class="box box">
+			  
+                <div class="box-header with-border">
+                  <h3 class="box-title">Portfolio Composition</h3>
+                  <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                  </div>
+                </div><!-- /.box-header -->
+				
+				
+                <div class="box-body">
+                  <div class="row">
+                    <div class="col-md-8">
+					
+                      <div class="chart">
+					  
+						  <table id="tableOverview" class="table table-bordered table-hover">
+							<thead>
+							  <tr>
+								<th>Name</th>
+								<th>Value (SEK)</th>
+								<th>Allocation</th>
+								<th>Normal</th>
+								<th>Diff</th>
+								<th>Min-Max</th>
+							  </tr>
+							</thead>
+							<tbody>
+							  <tr>
+								<td>Index</td>
+								<td><?php //echo $index_nav ?></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							  </tr>
+                              <?php //$pdata=GetCompositionTable($dtmax, $_SESSION['company'], $index_nav_num); ?>
+							<tbody>
+						  </table>
+							  
+                      </div><!-- /.chart-responsive -->
+					  
+                    </div><!-- /.col -->
+					
+                    <div class="col-md-4">
+					  <canvas id="pieChart" height="250"></canvas>
+                    </div><!-- /.col -->
+					
+                    <div class="col-md-4">
+                      <ul class="chart-legend clearfix">
+                        <li><i class="fa fa-circle-o text-red"></i> Equities</li>
+                        <li><i class="fa fa-circle-o text-light-blue"></i> Rates</li>
+                        <li><i class="fa fa-circle-o text-green"></i> Alternatives</li>
+                      </ul>
+                    </div><!-- /.col -->
+					
+                  </div><!-- /.row -->
+                </div><!-- ./box-body -->
+				
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+		  
+		  
+		  
+          <div class="row">
+            <div class="col-md-12">
+              <div class="box">
+			  
+                <div class="box-header with-border">
+                  <h3 class="box-title">Portfolio Performance</h3>
+                  <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                  </div>
+                </div><!-- /.box-header -->
+				
+				
+
+				
+                <div class="box-body">
+                  <div class="row">
+                    <div class="col-md-12">
+					
+                      <div class="table">
+					  
+					        <?php
+					            //echo GetDetailsTableSimple(array("Index", "Benchmark"), $dtmin, $dtmax);
+					        ?>
+					        
+                      </div><!-- /.chart-responsive -->
+                    </div><!-- /.col -->
+					
+					
+                  </div><!-- /.row -->
+                  <div class="row">
+                    <div class="col-md-12">
+					  <canvas id="areaChart" height="200"></canvas>
+                    </div><!-- /.col -->
+                  </div><!-- /.row -->
+					  
+                </div><!-- ./box-body -->
+				
+				
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+		  
+				
+          <div class="row">
+            <div class="col-md-6">
+              <div class="box box-primary">
+		  
+                <div class="box-header with-border">
+                  <h3 class="box-title">Cash Management</h3>
+                  <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                  </div>
+                </div><!-- /.box-header -->
+				
+                <div class="box-body">
+                  <div class="row">
+                    <div class="col-md-12">
+					
+                      <div class="table">
+					  
+						  <table id="tableCashManagement" class="table table-bordered table-hover">
+							<thead>
+							  <tr>
+								<th>Date</th>
+								<th>Instrument</th>
+								<th>Amount</th>
+							  </tr>
+							</thead>
+							<tbody>
+							  <tr>
+								<td>12-Jun-2015</td>
+								<td>BATSJ 13 06/12/17</td>
+								<td>712,589</td>
+							  </tr>
+							  <tr>
+								<td>26-Jun-2015</td>
+								<td>STERV 5 3/4 06/26/17</td>
+								<td>292,644</td>
+							  </tr>
+							  <tr>
+								<td>27-Jun-2015</td>
+								<td>SCANDA 8 1/2 06/27/18</td>
+								<td>425,000</td>
+							  </tr>
+							  <tr>
+								<td>28-Jun-2015</td>
+								<td>OPM Listed Private Equity</td>
+								<td>627,000</td>
+							  </tr>
+							<tbody>
+						  </table>
+						  
+					  </div> <!-- /.table -->
+                    </div><!-- class="col-md-6"> -->
+                  </div><!-- class="row"> -->
+                </div><!-- class="box-body"> -->
+					  
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+			
+			
+			
+            <div class="col-md-6">
+              <div class="box box-info">
+		  
+                <div class="box-header with-border">
+                  <h3 class="box-title">Winners/Losers</h3>
+                  <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                  </div>
+                </div><!-- /.box-header -->
+				
+                <div class="box-body">
+                  <div class="row">
+                    <div class="col-md-12">
+				
+                      <div class="table">
+						  <table id="tableWinners" class="table table-bordered table-hover">
+							<thead>
+							  <tr>
+								<th>Instrument</th>
+								<th>NAV</th>
+								<th>Change</th>
+							  </tr>
+							</thead>
+							<tbody>
+							  <tr>
+								<td><span class="description-percentage text-green"><i class="fa fa-caret-up"></i> Swedbank Robur Penningmarknads</span></td>
+								<td><span class="description-percentage text-green">64,047,833</span></td>
+								<td><span class="description-percentage text-green">269,001</span></td>
+							  </tr>
+							  <tr>
+								<td><span class="description-percentage text-green"><i class="fa fa-caret-up"></i> Swedbank Robur Penningmarknads</span></td>
+								<td><span class="description-percentage text-green">16,104,449</span></td>
+								<td><span class="description-percentage text-green">144,287</span></td>
+							  </tr>
+							  <tr>
+								<td><span class="description-percentage text-red"><i class="fa fa-caret-down"></i> SEB Sverige Stiftelsefond</span></td>
+								<td><span class="description-percentage text-red">63,194,905</span></td>
+								<td><span class="description-percentage text-red">-97,952</span></td>
+							  </tr>
+							  <tr>
+								<td><span class="description-percentage text-red"><i class="fa fa-caret-down"></i> SEB Foretagsobligationsfond Fl</span></td>
+								<td><span class="description-percentage text-red">45,258,816</span></td>
+								<td><span class="description-percentage text-red">-133,514</span></td>
+							  </tr>
+							<tbody>
+						  </table>
+					  </div> <!-- /.table -->
+					  
+                    </div><!-- class="col-md-6"> -->
+                  </div><!-- class="row"> -->
+                </div><!-- class="box-body"> -->
+				
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+		  
+        </section>
+		
+	
+	
+    <script>
+      $(function () {
+        /* ChartJS
+         * -------
+         * Here we will create a few charts using ChartJS
+         */
+		 
+		 
+        //--------------
+        //- AREA CHART -
+        //--------------
+
+        // Get context with jQuery - using jQuery's .get() method.
+        var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
+        // This will get the first returned node in the jQuery collection.
+        var areaChart = new Chart(areaChartCanvas);
+        
+
+        <?php
+            //echo GetDetailsChart("areaChartData", array("Index", "Benchmark"));
+        ?>
+
+
+        var areaChartOptions = {
+          //Boolean - If we should show the scale at all
+          showScale: true,
+          //Boolean - Whether grid lines are shown across the chart
+          scaleShowGridLines: false,
+          //String - Colour of the grid lines
+          scaleGridLineColor: "rgba(0,0,0,.05)",
+          //Number - Width of the grid lines
+          scaleGridLineWidth: 1,
+          //Boolean - Whether to show horizontal lines (except X axis)
+          scaleShowHorizontalLines: true,
+          //Boolean - Whether to show vertical lines (except Y axis)
+          scaleShowVerticalLines: true,
+          //Boolean - Whether the line is curved between points
+          bezierCurve: true,
+          //Number - Tension of the bezier curve between points
+          bezierCurveTension: 0.3,
+          //Boolean - Whether to show a dot for each point
+          pointDot: false,
+          //Number - Radius of each point dot in pixels
+          pointDotRadius: 4,
+          //Number - Pixel width of point dot stroke
+          pointDotStrokeWidth: 1,
+          //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+          pointHitDetectionRadius: 20,
+          //Boolean - Whether to show a stroke for datasets
+          datasetStroke: true,
+          //Number - Pixel width of dataset stroke
+          datasetStrokeWidth: 2,
+          //Boolean - Whether to fill the dataset with a color
+          datasetFill: false,
+          //String - A legend template
+          legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+          //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+          maintainAspectRatio: false,
+          //Boolean - whether to make the chart responsive to window resizing
+          responsive: true
+        };
+
+        //Create the line chart
+        areaChart.Line(areaChartData, areaChartOptions);
+
+		 
+        //-------------
+        //- PIE CHART -
+        //-------------
+        // Get context with jQuery - using jQuery's .get() method.
+        var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
+        var pieChart = new Chart(pieChartCanvas);
+
+          <?php //echo $pdata; ?>
+
+        var pieOptions = {
+          //Boolean - Whether we should show a stroke on each segment
+          segmentShowStroke: true,
+          //String - The colour of each segment stroke
+          segmentStrokeColor: "#fff",
+          //Number - The width of each segment stroke
+          segmentStrokeWidth: 2,
+          //Number - The percentage of the chart that we cut out of the middle
+          percentageInnerCutout: 30, // This is 0 for Pie charts
+          //Number - Amount of animation steps
+          animationSteps: 100,
+          //String - Animation easing effect
+          animationEasing: "easeOutBounce",
+          //Boolean - Whether we animate the rotation of the Doughnut
+          animateRotate: true,
+          //Boolean - Whether we animate scaling the Doughnut from the centre
+          animateScale: false,
+          //Boolean - whether to make the chart responsive to window resizing
+          responsive: true,
+          // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+          maintainAspectRatio: false,
+          //String - A legend template
+          legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+        };
+        //Create pie or douhnut chart
+        //pieChart.Doughnut(PieData, pieOptions);
+        pieChart.Pie(PieData, pieOptions);
+
+      });
+    </script>
+	
+
--- themes/abound/views/layouts/main.php
+++ themes/abound/views/layouts/main.php
@@ -28,13 +28,24 @@
 	  $cs->registerCssFile($baseUrl.'/css/bootstrap-responsive.min.css');
       
       $cs->registerCssFile($baseUrl.'/css/animate.min.css');
-      $cs->registerCssFile($baseUrl.'/css/font-awesome.min.css');
+      //$cs->registerCssFile($baseUrl.'/css/font-awesome.min.css');
       $cs->registerCssFile($baseUrl.'/css/templatemo-style.css');
+      
+      //$cs->registerCssFile($baseUrl.'/css/ionicons.min.css');
+      
+      $cs->registerCssFile($baseUrl.'/css/AdminLTE.min.css');
       
 	  $cs->registerCssFile($baseUrl.'/css/abound.css');
 	  //$cs->registerCssFile($baseUrl.'/css/style-blue.css');
 	  ?>
-      <!-- styles for style switcher -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+      	<!-- jvectormap 
+		<link href="<?php //echo $baseUrl;?>/css/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />-->
+        <!-- Ionicons 
+		<link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />-->
+        
+      <!-- styles for style switcher 
+      <link rel="stylesheet" href="dist/css/AdminLTE.min.css">-->
         <!--<link rel="stylesheet" type="text/css" href="<?php //echo $baseUrl;?>/datatables/media/css/jquery.dataTables.css" />-->
         <!--<link href="plugins/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />-->
         <!-- animate css 
@@ -48,12 +59,18 @@
 		<!-- custom css 
 		<link rel="stylesheet" href="<?php //echo $baseUrl;?>/css/templatemo-style.css">-->
 
+     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
 	  <?php
 	  $cs->registerScriptFile($baseUrl.'/js/bootstrap.min.js');
-	  //$cs->registerScriptFile($baseUrl.'/js/plugins/jquery.sparkline.js');
+	  $cs->registerScriptFile($baseUrl.'/js/plugins/jquery.sparkline.js');
 	  //$cs->registerScriptFile($baseUrl.'/js/plugins/jquery.flot.min.js');
 	  //$cs->registerScriptFile($baseUrl.'/js/plugins/jquery.flot.pie.min.js');
-	  //$cs->registerScriptFile($baseUrl.'/js/charts.js');
+	  $cs->registerScriptFile($baseUrl.'/js/charts.js');
 	  //$cs->registerScriptFile($baseUrl.'/js/plugins/jquery.knob.js');
 	  //$cs->registerScriptFile($baseUrl.'/js/plugins/jquery.masonry.min.js');
 	  //$cs->registerScriptFile($baseUrl.'/js/styleswitcher.js');
@@ -61,6 +78,7 @@
       $cs->registerScriptFile($baseUrl.'/js/wow.min.js');
 	  $cs->registerScriptFile($baseUrl.'/js/jquery.singlePageNav.min.js');
 	  //$cs->registerScriptFile($baseUrl.'/js/custom.js');
+      $cs->registerScriptFile($baseUrl.'/js/app.min.js');
 	?>
   </head>
 <body>

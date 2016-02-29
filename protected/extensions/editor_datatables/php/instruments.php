a1c733e7f05cf8d28de50296f045efeac052de07
artmart <artrmart@gmail.com>
2016-03-01 00:31:50 +0400

    fix

--- protected/extensions/editor_datatables/php/instruments.php
+++ protected/extensions/editor_datatables/php/instruments.php
@@ -46,12 +46,12 @@
  */       
         
 // Build our Editor instance and process the data coming from _POST
-Editor::inst( $db, 'instruments')
+Editor::inst( $db, 'instruments', 'instruments.id')
     ->fields(
         Field::inst( 'instrument_types.instrument_type' ),
         Field::inst( 'instrument_groups.group_name' ),
-        Field::inst( 'instruments.id' ),
-        Field::inst( 'instruments.instrument' ),
+        Field::inst( 'instruments.id as id' ),
+        Field::inst( 'instruments.instrument as instrument' ),
         Field::inst( 'instruments.instrument_type_id' ),
         Field::inst( 'instruments.is_current' ),
         Field::inst( 'instruments.created_at' ),

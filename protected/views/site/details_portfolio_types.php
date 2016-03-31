f217c5f44adb4c1af44eedf6eb9a942df0ff083b
artmart <artrmart@gmail.com>
2016-03-31 22:25:40 +0400

    details

--- protected/views/site/details_portfolio_types.php
+++ protected/views/site/details_portfolio_types.php
@@ -48,37 +48,47 @@
     $table1_results = Yii::app()->db->createCommand($sql_table1)->queryAll(true);
     */
     
-    $sql_table1 = "select * from portfolio_returns where portfolio_id = 1 order by trade_date";
-    $table1_results = Yii::app()->db->createCommand($sql_table1)->queryAll(true);
-    
+    $sql_returns = "select * from portfolio_returns where portfolio_id = '$portfolio_id' order by trade_date";
+    $portfolio_results = Yii::app()->db->createCommand($sql_returns)->queryAll(true);
+    if($portfolio_results){
+    foreach($portfolio_results as $pr){
+        $port_ret[] = $pr['return'];
+        $bench_ret[] = $pr['benchmark_return'];
+        
+        $port_data[] = [$pr['trade_date'], floatval($pr['return'])];
+        $bench_data[] = [$pr['trade_date'], floatval($pr['benchmark_return'])];
+        
+        
+    }
+    $allstats = Calculators::CalcAllStats1($port_ret, $bench_ret);
     
     
   $tbl_rows .=   
     '<tr>
         <td>'. $portfolio['portfolio'].'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
-        <td>'. number_format(11).'</td>
+        <td>'. number_format(1111).'</td>
+        <td>'. number_format(1111).'</td>
+        <td>'. number_format(1111).'</td>
+        <td>'. number_format(1111).'</td>
+        <td>'. number_format(1111).'</td>
+        <td>'. number_format(1111).'</td>
+        <td>'. number_format($allstats[0], 3).'</td>
+        <td>'. number_format($allstats[1], 3).'</td>
+        <td>'. number_format($allstats[2], 3).'</td>
+        <td>'. number_format($allstats[4], 3).'</td>
+        <td>'. number_format($allstats[13], 3).'</td>
+        <td>'. number_format($allstats[14], 3).'</td>
+        <td>'. number_format($allstats[3], 3).'</td>
+        <td>'. number_format($allstats[5], 3).'</td>
+        <td>'. number_format($allstats[6], 3).'</td>
+        <td>'. number_format($allstats[7], 3).'</td>
+        <td>'. number_format($allstats[9], 3).'</td>
+        <td>'. number_format($allstats[8], 3).'</td>
+        <td>'. number_format($allstats[10], 3).'</td>
+        <td>'. number_format($allstats[11], 3).'</td>
+        <td>'. number_format($allstats[12], 3).'</td>
     </tr>';
-    
+    }
 }
 
 
@@ -175,7 +185,8 @@ $(function () {
             title: {
                 text: ''// 'Snow depth (m)'
             },
-            min: 0
+            min: 0.925,
+            max: 1.04
         },
         tooltip: {
             headerFormat: '<b>{series.name}</b><br>',
@@ -202,69 +213,10 @@ $(function () {
             // Define the data points. All series have a dummy year
             // of 1970/71 in order to be compared on the same x axis. Note
             // that in JavaScript, months start at 0 for January, 1 for February etc.
-            data: [
-                [Date.UTC(1970, 9, 21), 0],
-                [Date.UTC(1970, 10, 4), 0.28],
-                [Date.UTC(1970, 10, 9), 0.25],
-                [Date.UTC(1970, 10, 27), 0.2],
-                [Date.UTC(1970, 11, 2), 0.28],
-                [Date.UTC(1970, 11, 26), 0.28],
-                [Date.UTC(1970, 11, 29), 0.47],
-                [Date.UTC(1971, 0, 11), 0.79],
-                [Date.UTC(1971, 0, 26), 0.72],
-                [Date.UTC(1971, 1, 3), 1.02],
-                [Date.UTC(1971, 1, 11), 1.12],
-                [Date.UTC(1971, 1, 25), 1.2],
-                [Date.UTC(1971, 2, 11), 1.18],
-                [Date.UTC(1971, 3, 11), 1.19],
-                [Date.UTC(1971, 4, 1), 1.85],
-                [Date.UTC(1971, 4, 5), 2.22],
-                [Date.UTC(1971, 4, 19), 1.15],
-                [Date.UTC(1971, 5, 3), 0]
-            ]
+            data: <?php echo json_encode($port_data); ?>
         }, {
             name: 'Swedish Equities',
-            data: [
-                [Date.UTC(1970, 9, 29), 0],
-                [Date.UTC(1970, 10, 9), 0.4],
-                [Date.UTC(1970, 11, 1), 0.25],
-                [Date.UTC(1971, 0, 1), 1.66],
-                [Date.UTC(1971, 0, 10), 1.8],
-                [Date.UTC(1971, 1, 19), 1.76],
-                [Date.UTC(1971, 2, 25), 2.62],
-                [Date.UTC(1971, 3, 19), 2.41],
-                [Date.UTC(1971, 3, 30), 2.05],
-                [Date.UTC(1971, 4, 14), 1.7],
-                [Date.UTC(1971, 4, 24), 1.1],
-                [Date.UTC(1971, 5, 10), 0]
-            ]
-        }, {
-            name: 'Intl Equities',
-            data: [
-                [Date.UTC(1970, 10, 25), 0],
-                [Date.UTC(1970, 11, 6), 0.25],
-                [Date.UTC(1970, 11, 20), 1.41],
-                [Date.UTC(1970, 11, 25), 1.64],
-                [Date.UTC(1971, 0, 4), 1.6],
-                [Date.UTC(1971, 0, 17), 2.55],
-                [Date.UTC(1971, 0, 24), 2.62],
-                [Date.UTC(1971, 1, 4), 2.5],
-                [Date.UTC(1971, 1, 14), 2.42],
-                [Date.UTC(1971, 2, 6), 2.74],
-                [Date.UTC(1971, 2, 14), 2.62],
-                [Date.UTC(1971, 2, 24), 2.6],
-                [Date.UTC(1971, 3, 2), 2.81],
-                [Date.UTC(1971, 3, 12), 2.63],
-                [Date.UTC(1971, 3, 28), 2.77],
-                [Date.UTC(1971, 4, 5), 2.68],
-                [Date.UTC(1971, 4, 10), 2.56],
-                [Date.UTC(1971, 4, 15), 2.39],
-                [Date.UTC(1971, 4, 20), 2.3],
-                [Date.UTC(1971, 5, 5), 2],
-                [Date.UTC(1971, 5, 10), 1.85],
-                [Date.UTC(1971, 5, 15), 1.49],
-                [Date.UTC(1971, 5, 23), 1.08]
-            ]
+            data: <?php echo json_encode($bench_data); ?>
         }]
     });
 });

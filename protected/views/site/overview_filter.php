822c93f96a46ab202bc30bbf87b653d19a957b0a
artmart <artrmart@gmail.com>
2016-06-13 00:46:01 +0400

    fix

--- protected/views/site/overview_filter.php
+++ protected/views/site/overview_filter.php
@@ -520,11 +520,15 @@ $(function () {
 							</thead>
 							<tbody>
                       <?php                       
-                        $cf_sql = "select distinct cf.cash_flow_date, i.instrument, cf.cash_flow, cft.cash_flow_type from cash_flows cf
+                        $cf_sql = "select cf.cash_flow_date, i.instrument, sum(cf.cash_flow * l.nominal*cr.{$portfolio_currency}/curs.cur_rate) cash_flow, cft.cash_flow_type from cash_flows cf
                                 inner join cash_flow_types cft on cf.`type` = cft.id
                                 inner join instruments i on i.id = cf.instrument
                                 inner join ledger l on l.instrument_id = i.id
+                                inner join currency_rates cr on cr.day = l.trade_date
+                                inner join cur_rates curs on curs.day = cf.cash_flow_date and curs.cur = i.currency
                                 where cf.cash_flow_date>='$end_date' and l.is_current = 1 and l.trade_status_id = 2 and l.portfolio_id = '$portfolio'
+                                and l.client_id ='$client_id'
+                                group by cf.cash_flow_date, i.instrument
                                 limit 6";
                         $cf_results = Yii::app()->db->createCommand($cf_sql)->queryAll(true);
                         foreach($cf_results as $cf){
